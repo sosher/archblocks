6eb5d8451afdd4f1ea64393152a8a47c1afe0210
puzzlepeaches <puzzlepeaches@users.noreply.github.com>
2016-02-25 08:29:34 -0600

    Update lenovo_thinkpad_x220.sh

--- blocks/hardware/laptop/lenovo_thinkpad_x220.sh
+++ blocks/hardware/laptop/lenovo_thinkpad_x220.sh
@@ -1,59 +1,131 @@
 #!/bin/bash
 
-# current x220 related archlinux tweaks/patches
+#tweaks for macbook pro 12,1
+#Hardware Overview:
+  #Model Name:MacBook Pro
+  #Model Identifier:MacBookPro12,1
+  #Processor Name:Intel Core i5
+  #Processor Speed:2.9 GHz
+  #Number of Processors:1
+  #Total Number of Cores:2
+  #L2 Cache (per Core):256 KB
+  #L3 Cache:3 MB
+  #Memory:8 GB
+  #Boot ROM Version:MBP121.0167.B15
+  #SMC Version (system):2.28f7
+  #Serial Number (system):C2QQ305MGKJN
+  #Hardware UUID:18C6FD43-97C5-53DD-B54F-0FCAB8C2CDEF
+#Audio 
+    #Intel High Def
+    #Mic/Speakers Sample Rate: 44100
+    #Manufacturer: Apple Inc. 
+#Blutooth
+  #Apple Bluetooth Software Version:4.4.3f4 16616
+  #Address:	A4-5E-60-E1-66-F6
+  #Manufacturer:Broadcom
+  #Transport:USB
+  #Chipset:	20703A1
+  #Firmware Version:v114 c4548
+  #Vendor ID:0x05AC
+  #Product ID:0x8290
+  #HCI Version:0x7
+  #HCI Revision:0x11C4
+  #LMP Version:0x7
+  #LMP Subversion:0x2172
+#Camera
+  #Model ID:Apple Camera VendorID_0x106B ProductID_0x1570
+  #Unique ID:DJH5115TQG9G1HPBG
+#Built in SD Card Reader:
+  #Vendor ID:0x05ac
+  #Product ID:0x8406
+  #Revision:3.00
+  #Serial Number:820
+#Display
+  #Chipset Model:Intel Iris Graphics 6100
+  #Type:GPU
+  #Bus:Built-In
+  #VRAM (Dynamic, Max):1536 MB
+  #Vendor:Intel (0x8086)
+  #Device ID:0x162b
+  #Revision ID:0x0009
+  #Display Type:	Retina LCD
+  #Resolution:2560 x 1600 Retina
+  #Retina:Yes
+  #Pixel Depth:32-Bit Color (ARGB8888)
+#Memory
+    #BANK 0/DIMM0:
+        #Size:4 GB
+        #Type:DDR3
+        #Speed:1867 MHz
+        #Manufacturer:0x80AD
+        #Part Number:0x483943434E4E4E424C54414C41522D4E5544
+    #BANK 1/DIMM0:
+        #Size:4 GB
+        #Type:DDR3
+        #Speed:1867 MHz
+        #Manufacturer:0x80AD
+        #Part Number:0x483943434E4E4E424C54414C41522D4E5544
+#Apple SSD Controller:
+  #Vendor:Apple
+  #Product:SSD Controller
+  #Physical Interconnect:PCI
+  #Link Width:x4
+  #Link Speed:5.0 GT/s
+  #Description:AHCI Version 1.30 Supported
+    #APPLE SSD SM0512G:
+        #Capacity:500.28 GB (500,277,790,720 bytes)
+        #Model:APPLE SSD SM0512G                       
+        #Revision:BXW1PA0Q
+        #Serial Number:S29ANYAG436062      
+        #Native Command Queuing:Yes
+        #Queue Depth:32
+        #BSD Name:	disk0
+        #TRIM Support:	Yes
+        #Partition Map Type:GPT (GUID Partition Table)
+    #EFI:
+        #Capacity:209.7 MB (209,715,200 bytes)
+        #BSD Name:disk0s1
+        #Content:EFI
+        #Volume UUID:0E239BC6-F960-3107-89CF-1C97F78BB46B
+    #disk0s2:
+        #Capacity:499.42 GB (499,418,034,176 bytes)
+        #BSD Name:disk0s2
+        #Content:Apple_CoreStorage
+    #Recovery HD:
+        #Capacity:650 MB (650,002,432 bytes)
+        #BSD Name:disk0s3
+        #Content:Apple_Boot
+        #Volume UUID:50730ED1-973B-358A-9671-6393A55817FD
+#Trackpad/Keyboard
+  #Product ID:0x0273
+  #Vendor ID:0x05ac (Apple Inc.)
+  #Version:6.22
+  #Serial Number:D3H526671Z1FTV3AE5NF
+  #Manufacturer:Apple Inc.
+  #Location ID:0x01000000
+#USB 3
+  #Host Controller Driver:AppleUSBXHCIWPT
+  #PCI Device ID:0x9cb1 
+  #PCI Revision ID:0x0003 
+  #PCI Vendor ID:0x8086
+#Wi-Fi
+    #Software Versions:
+        #CoreWLAN:11.0 (1101.20)
+        #CoreWLANKit:11.0 (1101.20)
+        #Menu Extra:11.0 (1110.33)
+        #System Information:11.0 (1100.1)
+        #IO80211 Family:11.1 (1110.26)
+        #Diagnostics:5.1 (510.88)
+        #AirPort Utility:6.3.6 (636.5)
+    #Interfaces:
+        #en0:
+            #Card Type:AirPort Extreme  (0x14E4, 0x133)
+            #Firmware Version:Broadcom BCM43xx 1.0 (7.21.94.136.1a1)
+            #MAC Address:a4:5e:60:e1:66:f5
+            #Locale:FCC
+            #Country Code:US
+            #Supported PHY Modes:802.11 a/b/g/n/ac
 
-# watchdog error at boot: (itco_wdt cannot register miscdev on minor)
-# https://bbs.archlinux.org/viewtopic.php?id=133083
-
-# echo blacklist mei > /etc/modprobe.d/mei.conf
 
 
 
-# boot error: Unknown user uuidd
-# _anykey "Please reinstall util-linux after reboot to eliminate uuidd related error"
-# pacman -Syu util-linux
-
-
-_installaur thinkfan
-sudo systemctl enable thinkfan.service
-echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkfan.conf
-
-# enable blinking wifi led
-cat > /etc/modprobe.d/iwlwifi.conf << EOF
-# led_mode=0 - led_mode:0=system default, 1=On(RF On)/Off(RF Off), 2=blinking, 3=Off (default: 0) (int)
-options iwlwifi led_mode=2
-options iwlwifi power_save=1
-EOF
-
-# ACPI HANDLER --------------------------------------------------------
-mv /etc/acpi/handler.sh /etc/acpi/handler.sh.prethinkpad
-cat > /etc/acpi/handler.sh << EOF
-#!/bin/bash
-
-set $*
-
-case $1 in
-    ac_adapter) power auto ;; # unplug or plug ac cord
-esac
-
-case $2 in
-    LID)     [ ${3:-} == "open" ] || system sleep ;;
-    TBLT)    [ ${3:-} == "on" ] && display tablet on || display tablet off ;;
-esac
-
-case $4 in
-    #0000500c) trigger trigger XF86_LaunchA ;; # undock tablet pen
-    #0000500b) trigger shift+LaunchA        ;; # dock tablet pen
-esac ;;
-
-EOF
-
-# SYSTEMD LOGIND.CONF -------------------------------------------------
-
-# we're going to handle power events in acpi for now
-cat >> /etc/systemd/logind.conf << EOF
-HandlePowerKey=ignore
-HandleSuspendKey=ignore
-HandleHibernateKey=ignore
-HandleLidSwitch=ignore
-EOF
